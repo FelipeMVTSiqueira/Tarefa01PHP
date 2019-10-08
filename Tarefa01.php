2a0936509f3ca07a5d2a4877c895e18546e92113
Felipe Siqueira <felipesiqueira1212@gmail.com>
2019-10-08 11:38:05 -0300

    mais um pouco da tarefa01 testando o git tbm

--- Tarefa01.php
+++ Tarefa01.php
@@ -1,2 +1,48 @@
-<?php echo "Hello World!";?>
+<?php echo "Hello World!"."<br>";?>
 
+<?php 
+    $numInt = 9;
+    echo "Um numero inteiro. Exemplo: ".$numInt."<br>";
+?>
+
+<?php
+    $numDec = 2.69;
+    echo "Um numero decimal. Exemplo: ".$numDec."<br>";
+?>
+
+<?php 
+    $var1crct = "P";
+    echo "Uma variável de um caractere usando aspas duplas: ".$var1crct."<br>";
+?>
+
+<?php 
+    $var1crct2 = 'A';
+    echo "Uma variável de um caractere usando aspas simples: ".$var1crct2."<br>";
+?>
+
+<?php 
+    $var1strg = 'Eu acho que isso é uma string';
+    echo $var1strg."<br>";
+?>
+
+<?php 
+    $var1strg = "Eu acho que isso é uma string";
+    echo $var1strg."<br>";
+?>
+
+<?php 
+    $um = "Três";
+    $dois = "pratos";
+    $tres = "de";
+    $quatro = "trigo";
+    $cinco = "para";
+    $seis = "tres";
+    $sete = "tigres";
+    $oito = "tristes";
+
+    echo $um." ".$dois." ".$tres." ".$quatro." ".$cinco." ".$seis." ".$sete." ".$oito."<br>";
+?>
+
+<?php 
+    echo $um." ".$oito." ".$sete." ".$tres." ".$quatro;
+?>
